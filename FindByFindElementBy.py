d0dc981d25982283fa394f8f3dc8e2e65eeb3177
bwielk <bwielk@gmail.com>
2018-12-08 20:25:23 +0000

    Finding elements using BY

--- FindByFindElementBy.py
+++ FindByFindElementBy.py
@@ -10,9 +10,15 @@ class FindByFindElementBy():
         driver = webdriver.Chrome(driver_location)
         driver.get("https://learn.letskodeit.com/p/practice")
 
+        dictionary_of_elements = {}
+        dictionary_of_elements["elements_retrieved_by_id"] = list(driver.find_elements(By.ID, "openwindow"))
+        dictionary_of_elements["elements_retrieved_by_fieldset_tag"] = list(driver.find_elements(By.TAG_NAME, "fieldset"))
+        dictionary_of_elements["elements_retrieved_by_link_text"] = list(driver.find_elements(By.LINK_TEXT, "Sign Up"))
+        dictionary_of_elements["elements_retrieved_by_xpath"] = list(driver.find_elements(By.XPATH, "//*[@id='block-1069048']"))
+        dictionary_of_elements["elements_retrieved_by_name"] = list(driver.find_elements(By.NAME, "cars"))
 
-        if element_by_id or element_by_name is not None:
-            print("Element has been found")
+        for k,v in dictionary_of_elements.items():
+            print(k, "=>", len(v))
 
 
 chrome_aut = FindByFindElementBy()
