ba4bbd9fb92c05a61dad109cde3e8a4d6860b11c
bwielk <bwielk@gmail.com>
2018-12-09 22:25:53 +0000

    Managing checkboxes and radios

--- browser_interactions/radios_and_checkboxes.py
+++ browser_interactions/radios_and_checkboxes.py
@@ -6,6 +6,14 @@ from time import sleep
 
 class RadiosAndCheckboxes():
 
+    is_selected_checker_counter = 0
+
+    def _check_statues_of_checkboxes_and_radios(self, array_of_elements):
+        self.is_selected_checker_counter += 1
+        print("CHECKING SESSION NUMBER %s" % self.is_selected_checker_counter)
+        for element in array_of_elements:
+            print("%s is selected => %s" % (element.id, element.is_selected()))
+
     def test(self):
         driver_location = 'C:\\Users\\BlazejW\\selenium\\chromedriver.exe'
         os.environ['webdriver.chrome.driver'] = driver_location
@@ -21,17 +29,25 @@ class RadiosAndCheckboxes():
         benz_check = driver.find_element_by_id("benzcheck")
         honda_check = driver.find_element_by_id("hondacheck")
 
+        #ARRAY OF ALL ELEMENTS
+        all_selectable_elements = [bmw_radio_element, benz_radio_element, honda_radio_element, honda_check, benz_check, bmw_check]
+
         bmw_radio_element.click()
         bmw_check.click()
+        self._check_statues_of_checkboxes_and_radios(all_selectable_elements)
         sleep(2)
         benz_radio_element.click()
         benz_check.click()
+        self._check_statues_of_checkboxes_and_radios(all_selectable_elements)
         sleep(3)
         bmw_radio_element.click()
         honda_check.click()
+        self._check_statues_of_checkboxes_and_radios(all_selectable_elements)
         sleep(3)
         honda_radio_element.click()
+        self._check_statues_of_checkboxes_and_radios(all_selectable_elements)
         sleep(3)
+        self.is_selected_checker_counter = 0
 
 chrome_aut = RadiosAndCheckboxes()
 chrome_aut.test()
