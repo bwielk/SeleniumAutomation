e7db22375276807eb227aebbcd10054e6f0e9cf7
bwielk <bwielk@gmail.com>
2018-12-15 20:09:37 +0000

    Invisible elements

---
+++ browser_interactions/invisible_elements.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+import os
+from time import sleep
+from selenium.webdriver.support.select import Select
+
+class InvisibleElements():
+
+    def test_lets_kode_it(self):
+        driver_location = 'C:\\Users\\BlazejW\\selenium\\chromedriver.exe'
+        os.environ['webdriver.chrome.driver'] = driver_location
+        driver = webdriver.Chrome(driver_location)
+        driver.get("https://learn.letskodeit.com/p/practice")
+        driver.maximize_window()
+
+        text_box_element = driver.find_element_by_id('displayed-text')
+        show_button = driver.find_element_by_id('show-textbox')
+        hide_button = driver.find_element_by_id('hide-textbox')
+
+        show_button.click()
+        driver.implicitly_wait(5)
+        self._click_visible_or_invisible_lets_kode_it(text_box_element, hide_button, show_button)
+
+        hide_button.click()
+        driver.implicitly_wait(5)
+        self._click_visible_or_invisible_lets_kode_it(text_box_element, hide_button, show_button)
+
+
+    def _click_visible_or_invisible_lets_kode_it(self, text_box_element, hide_button, show_button):
+        if text_box_element.is_displayed():
+            hide_button.click()
+        else:
+            show_button.click()
+
+    def test_expedia(self):
+        driver_location = 'C:\\Users\\BlazejW\\selenium\\chromedriver.exe'
+        os.environ['webdriver.chrome.driver'] = driver_location
+        driver = webdriver.Chrome(driver_location)
+        driver.get("https://www.expedia.co.uk/")
+        driver.maximize_window()
+
+        baby_age_dropdown = driver.find_element_by_id('package-1-age-select-1-hp-package')
+        #baby_age_dropdown.click() Element not visible exception
+        print("The baby aga dropdown is visible = " + str(baby_age_dropdown.is_displayed()))
+        print(baby_age_dropdown.is_displayed())
+
+chrome_aut = InvisibleElements()
+chrome_aut.test_lets_kode_it()
+chrome_aut.test_expedia()
