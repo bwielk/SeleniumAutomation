7039ffc1a073fc6d8605d9890d733d8fc9ce8285
bwielk <bwielk@gmail.com>
2018-12-15 20:52:14 +0000

    Reading element's properties

--- browser_interactions/extra_props.py
+++ browser_interactions/extra_props.py
@@ -16,9 +16,18 @@ class ExtraProperties():
         open_tab_element_text = open_tab_element.text
         print('text on element is %s' % open_tab_element_text)
 
-
+    def get_value_attribute(self, element_id, attribute):
+        driver = self._prepare_driver()
+        element = driver.find_element_by_id(element_id)
+        result = element.get_attribute(attribute)
+        print('%s of element with id: %s is %s' % (attribute, element_id, result))
+        driver.quit()
 
 chrome_aut = ExtraProperties()
 chrome_aut.get_text()
+chrome_aut.get_value_attribute('name', 'type')
+chrome_aut.get_value_attribute('name', 'class')
+chrome_aut.get_value_attribute('name', 'value')
+
 
 
